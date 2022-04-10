79ba56c1d5678e68f7bcd6b5bb352c95618648e9
kainyzrwaw <sydney.wawrzyniak@kellogg.northwestern.edu>
2022-04-10 01:54:04 +0000

    Completed exercises

--- practice-6.sql
+++ practice-6.sql
@@ -15,4 +15,4 @@
 -- | Brighton Park          | 678      |
 -- | Burnside               | 10       |
 
-
+SELECT listings.neighborhood, COUNT(reviews.id) FROM reviews INNER JOIN listings ON listings.id = reviews.listing_id GROUP BY listings.neighborhood;
